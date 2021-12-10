8cbd93b488cae045152588a8d2b07f50e6e37384
William Polinchak <william.polinchak@windstream.com>
2021-12-10 09:09:35 -0500

    Adding rest service.

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+import logging
+from flask import Flask, request
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+log = logging.getLogger('main')
+
+# Port 8081
+app = Flask("pyProm")
+
+
+@app.route('/', methods=["GET"])
+def hi():
+    if request.method == "GET":
+        return "OK", 200, None
+
+    return "Bad Request", 400, None
+
+try:
+    if __name__ == '__main__':
+        app.run(host="0.0.0.0", port=8081)
+except RuntimeError:
+    log.info('shutdown')
