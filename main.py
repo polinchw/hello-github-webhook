a0d4e6ba494394529120f36d241b9eb5dba7752f
William Polinchak <william.polinchak@windstream.com>
2021-12-18 09:46:53 -0500

    fix(dev): Return hi there.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = Flask("pyProm")
 @app.route('/', methods=["GET"])
 def hi():
     if request.method == "GET":
-        return "hi", 200, None
+        return "hi there", 200, None
 
     return "Bad Request", 400, None
 
